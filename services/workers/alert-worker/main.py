67e7642006bd2c0d6ff107f176f0229b5a323584
atakang7 <atakan.gul000@gmail.com>
2025-12-26 17:42:42 +0300

    python change

--- services/workers/alert-worker/main.py
+++ services/workers/alert-worker/main.py
@@ -37,7 +37,7 @@ def parse_alert(msg: dict) -> dict:
 def main():
     log.info("Starting alert-worker")
     start_metrics_server(8000)
-
+ 
     log.info("Starting kafka-consumer")
     consumer = KafkaConsumer(
         topic=KAFKA_ALERTS_TOPIC,
